e033e64dd763a0e4ceec2182ffd95a2d62a8c834
jinlee-dev <jinlee.dev@gmail.com>
2024-09-09 08:30:15 +0900

    240909 PS : Binary Search

---
+++ bark/12_BinarySearch/BOJ_2467.py
@@ -0,0 +1,33 @@
+import sys
+n = int(sys.stdin.readline())
+liquids = list(map(int, sys.stdin.readline().split()))
+
+ans = float("INF")
+ans_left = 0
+ans_right = 0
+
+for i in range(n-1):
+    current = liquids[i]
+
+    start = i + 1
+    end = n - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        tmp = current + liquids[mid]
+
+        if abs(tmp) < ans:
+            ans = abs(tmp)
+            ans_left = i
+            ans_right = mid
+
+            if tmp == 0:
+                break
+        
+        if tmp < 0:
+            start = mid + 1
+        
+        else:
+            end = mid - 1
+
+print(liquids[ans_left], liquids[ans_right])
